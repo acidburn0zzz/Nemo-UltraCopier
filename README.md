16d32cdc84aa86c5e6d1644f07c3371cdefded93
lestcape <lestcape@gmail.com>
2014-05-09 08:33:28 -0500

    Inittial Release.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-Nemo-UltraCopy
+Nemo-UltraCopier-0.1
 ==============
 
-This is an action for the Nemo browser, to paste files using ultracopier # instead of the default nemo copier tool.
+Last version release date: 09 May 2014
+
+Author: [Lester Carballo Pérez](https://github.com/lestcape)
+
+
+This is an action for the Nemo browser, to paste files using ultracopier instead of the default nemo copier tool.
+
+
+Installation instruction
+==============
+
+1- Install ultracopier(http://ultracopier.first-world.info/)
+2- Download the nemo action files: https://github.com/lestcape/Nemo-UltraCopier/archive/master.zip
+3- Uncompress the file Nemo-UltraCopier.zip and copy the contents of 3  files to the place /home/USER/.local/share/nemo/actions where USER it's your user account.
+4- Test the result and enjoy.
---
+++ nemo-ultracopier.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python
+#  -*- coding: utf-8 -*-
+
+# Nemo action: Nemo-UltraCopier
+# Release Date: 09 May 2014
+#
+# Authors: Lester Carballo Pérez(https://github.com/lestcape).
+#
+#          Email: lestcape@gmail.com     Website: https://github.com/lestcape/Nemo-UltraCopier
+#
+# "This is an action for the Nemo browser, to paste files using ultracopier
+# instead of the default nemo copier tool."
+#
+# This program is free software:
+#
+#    You can redistribute it and/or modify it under the terms of the
+#    GNU General Public License as published by the Free Software
+#    Foundation, either version 3 of the License, or (at your option)
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import mimetypes
+import sys
+import os
+import urllib
+from gi.repository import Gtk, Gdk
+
+
+def normalize(path):
+    path = path.replace(" ", "\\ ")
+    path = path.replace("(", "\\(")
+    path = path.replace(")", "\\)")
+    path = path.replace("'", "\\'")
+    path = path.replace("&", "\\&")
+    path = urllib.unquote(path)
+    return path
+
+clipboard  = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+result = clipboard.wait_for_contents(Gdk.Atom.intern("x-special/gnome-copied-files", False))
+if result is not None:
+    info = result.get_data().splitlines()
+    action = info[0]
+    files = info[1:]
+    fileList = ""
+    for file in files:
+        if file.index("file://") == 0:
+            fileList += " " + normalize(file[7:])
+
+    print fileList
+    print sys.argv[1]
+    if action == "copy":
+        os.system("ultracopier cp %s %s" % (fileList, normalize(sys.argv[1])))
+    elif action == "cut":
+        os.system("ultracopier mv %s %s" % (fileList, normalize(sys.argv[1])))
+
